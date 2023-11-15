a143ad64e9f44ad765c1f4a99a22a5cc9a014dee
Kunyu Luo <47045853+kunyuluo@users.noreply.github.com>
2023-11-15 10:02:11 +0800

    Add files via upload

---
+++ absenteesim.py
@@ -0,0 +1,108 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Data Pre-processing
+# *************************************************************************************************************
+# Load data
+# *****************************************************************************************
+data = pd.read_csv('Absenteeism_data.csv')
+# print(data)
+
+df = data.copy()
+# print(df.info())
+
+df = df.drop(['ID'], axis=1)
+# print(df)
+
+# Create dummy variables for Reasons
+# *****************************************************************************************
+reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first=True)
+# reason_columns['check'] = reason_columns.sum(axis=1)
+# print(reason_columns['check'].sum(axis=0))
+
+# Group the Reasons for Absence
+# *****************************************************************************************
+df = df.drop(['Reason for Absence'], axis=1)
+
+reason_type1 = reason_columns.loc[:, 1:14].max(axis=1)
+reason_type2 = reason_columns.loc[:, 15:17].max(axis=1)
+reason_type3 = reason_columns.loc[:, 18:21].max(axis=1)
+reason_type4 = reason_columns.loc[:, 22:].max(axis=1)
+
+df = pd.concat([df, reason_type1, reason_type2, reason_type3, reason_type4], axis=1)
+
+# Rename the columns
+column_names = ['Date', 'Transportation Expense', 'Distance to Work', 'Age', 'Daily Work Load Average',
+                'Body Mass Index', 'Education', 'Children', 'Pets', 'Absenteeism Time in Hours', 'Reason_1', 'Reason_2',
+                'Reason_3', 'Reason_4']
+df.columns = column_names
+
+# Reorder the columns
+# *****************************************************************************************
+column_names_reordered = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4', 'Date', 'Transportation Expense',
+                          'Distance to Work', 'Age', 'Daily Work Load Average', 'Body Mass Index', 'Education',
+                          'Children', 'Pets', 'Absenteeism Time in Hours']
+df = df[column_names_reordered]
+
+# Reformat the 'Date'
+# *****************************************************************************************
+df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+# print(df['Date'][0])
+
+# Extract month value
+# *****************************************************************************************
+list_month = []
+for i in range(700):
+    list_month.append(df['Date'][i].month)
+
+df['Month Value'] = list_month
+
+
+# print(df)
+
+# Extract the Day of the Week
+# *****************************************************************************************
+def date_to_weekday(date_value):
+    return date_value.weekday()
+
+
+df['Day of the Week'] = df['Date'].apply(date_to_weekday)
+df.drop(['Date'], axis=1)
+# print(df.columns)
+column_names_reordered = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4', 'Month Value', 'Day of the Week',
+                          'Transportation Expense', 'Distance to Work', 'Age', 'Daily Work Load Average',
+                          'Body Mass Index', 'Education', 'Children', 'Pets', 'Absenteeism Time in Hours']
+df = df[column_names_reordered]
+
+# Create dummy variables for Educations (0 or 1)
+# *****************************************************************************************
+df['Education'] = df['Education'].map({1: 0, 2: 1, 3: 1, 4: 1})
+
+# Logistic Regression
+# *************************************************************************************************************
+# Create targets
+# *****************************************************************************************
+targets = np.where(df['Absenteeism Time in Hours'] > df['Absenteeism Time in Hours'].median(), 1, 0)
+df['Excessive Absenteeism'] = targets
+
+data_with_targets = df.drop(['Absenteeism Time in Hours'], axis=1)
+
+# Select inputs
+# *****************************************************************************************
+unscaled_inputs = data_with_targets.iloc[:, :-1]
+# print(unscaled_inputs.columns)
+
+# Standardize the data
+# *****************************************************************************************
+absenteeism_scaler = StandardScaler()
+absenteeism_scaler.fit(unscaled_inputs)
+scaled_inputs = absenteeism_scaler.transform(unscaled_inputs)
+# print(scaled_inputs.shape)
+
+# Split data into training and testing
+# *****************************************************************************************
+x_train, x_test, y_train, y_test = train_test_split(scaled_inputs, targets, train_size=0.8, random_state=20)
+# print(len(train_test_split(scaled_inputs, targets)[2]))
